e61b8817ac52b3b53f12ecffc6191cc330e90393
Cihangir SAVAS <cihangir@koding.com>
2014-09-07 20:44:18 -0700

    Cache: remove t/l operations from memory cache

--- memory.go
+++ memory.go
@@ -1,11 +1,6 @@
 package cache
 
-import (
-	"sync"
-	"time"
-)
-
-var zeroTTL = time.Duration(0)
+import "sync"
 
 type MemoryCache struct {
 	// Mutex is used for handling the concurrent
@@ -13,48 +8,15 @@ type MemoryCache struct {
 	sync.Mutex
 
 	// cache holds the cache data
-	cache *MemoryCacheNoTS
-
-	// setAts holds the time that related item's set at
-	setAts map[string]time.Time
-
-	// ttl is a duration for a cache key to expire
-	ttl time.Duration
-
-	// gcInterval is a duration for garbage collection
-	gcInterval time.Duration
+	cache Cache
 }
 
 // NewMemoryCache creates an inmemory cache system
 // Which everytime will return the true value about a cache hit
 func NewMemory() *MemoryCache {
-	return NewMemoryWithTTL(zeroTTL)
-}
-
-// NewMemoryCache creates an inmemory cache system
-// Which everytime will return the true values about a cache hit
-// and never will leak memory
-// ttl is used for expiration of a key from cache
-func NewMemoryWithTTL(ttl time.Duration) *MemoryCache {
 	return &MemoryCache{
-		cache:  NewMemoryNoTS(),
-		setAts: map[string]time.Time{},
-		ttl:    ttl,
+		cache: NewMemoryNoTS(),
 	}
-}
-
-// StartGC starts the garbage collection process in a go routine
-func (r *MemoryCache) StartGC(gcInterval time.Duration) {
-	r.gcInterval = gcInterval
-	go func() {
-		for _ = range time.Tick(gcInterval) {
-			for key, _ := range r.cache.items {
-				if !r.isValid(key) {
-					r.Delete(key)
-				}
-			}
-		}
-	}()
 }
 
 // Get returns a value of a given key if it exists
@@ -63,17 +25,7 @@ func (r *MemoryCache) Get(key string) (interface{}, error) {
 	r.Lock()
 	defer r.Unlock()
 
-	if !r.isValid(key) {
-		r.delete(key)
-		return nil, ErrNotFound
-	}
-
-	value, err := r.cache.Get(key)
-	if err != nil {
-		return nil, err
-	}
-
-	return value, nil
+	return r.cache.Get(key)
 }
 
 // Set will persist a value to the cache or
@@ -82,9 +34,7 @@ func (r *MemoryCache) Set(key string, value interface{}) error {
 	r.Lock()
 	defer r.Unlock()
 
-	r.cache.Set(key, value)
-	r.setAts[key] = time.Now()
-	return nil
+	return r.cache.Set(key, value)
 }
 
 // Delete deletes a given key if exists
@@ -92,24 +42,5 @@ func (r *MemoryCache) Delete(key string) error {
 	r.Lock()
 	defer r.Unlock()
 
-	r.delete(key)
-	return nil
-}
-
-func (r *MemoryCache) delete(key string) {
-	r.cache.Delete(key)
-	delete(r.setAts, key)
-}
-
-func (r *MemoryCache) isValid(key string) bool {
-	setAt, ok := r.setAts[key]
-	if !ok {
-		return false
-	}
-
-	if r.ttl == zeroTTL {
-		return true
-	}
-
-	return setAt.Add(r.ttl).After(time.Now())
+	return r.cache.Delete(key)
 }
