fec0bfcccce52c0d7070fafde3dd7db627f1ec81
Cihangir SAVAS <cihangir@koding.com>
2014-09-07 21:17:09 -0700

    Cache: rename MemoryCacheNoTS to MemoryNoTS

--- memory_nots.go
+++ memory_nots.go
@@ -1,21 +1,22 @@
 // Package cache provides basic caching mechanisms for Go(lang) projects.
 package cache
 
-type MemoryCacheNoTS struct {
+// MemoryNoTS provides a non-thread safe caching mechanism
+type MemoryNoTS struct {
 	// items holds the cache data
 	items map[string]interface{}
 }
 
-// NewMemoryNoTS creates an inmemory cache system
-func NewMemoryNoTS() *MemoryCacheNoTS {
-	return &MemoryCacheNoTS{
+// NewMemoryNoTS creates MemoryNoTS struct
+func NewMemoryNoTS() *MemoryNoTS {
+	return &MemoryNoTS{
 		items: map[string]interface{}{},
 	}
 }
 
 // Get returns a value of a given key if it exists
 // and valid for the time being
-func (r *MemoryCacheNoTS) Get(key string) (interface{}, error) {
+func (r *MemoryNoTS) Get(key string) (interface{}, error) {
 	value, ok := r.items[key]
 	if !ok {
 		return nil, ErrNotFound
@@ -26,14 +27,14 @@ func (r *MemoryCacheNoTS) Get(key string) (interface{}, error) {
 
 // Set will persist a value to the cache or
 // override existing one with the new one
-func (r *MemoryCacheNoTS) Set(key string, value interface{}) error {
+func (r *MemoryNoTS) Set(key string, value interface{}) error {
 	r.items[key] = value
 	return nil
 }
 
 // Delete deletes a given key, it doesnt return error if the item is not in the
 // system
-func (r *MemoryCacheNoTS) Delete(key string) error {
+func (r *MemoryNoTS) Delete(key string) error {
 	delete(r.items, key)
 	return nil
 }
