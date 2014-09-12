caae195b0c190c28b9126211518d4dc7aec01075
Cihangir SAVAS <cihangir@koding.com>
2014-09-12 00:00:45 -0700

    Cache: fixed typo in doc.go

--- doc.go
+++ doc.go
@@ -5,5 +5,5 @@
 //     Memory    : provides a thread safe in-memory caching system, built on top of MemoryNoTS cache
 //     LRUNoTS   : provides a non-thread safe, fixed size in-memory caching system, built on top of MemoryNoTS cache
 //     LRU       : provides a thread safe, fixed size in-memory caching system, built on top of LRUNoTS cache
-//     MemoryTTL : provides a thread safe, expring in-memory caching system,  built on top of MemoryNoTS cache
+//     MemoryTTL : provides a thread safe, expiring in-memory caching system,  built on top of MemoryNoTS cache
 package cache
