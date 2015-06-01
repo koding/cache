71a47795771917a13cd3d2e3e5fe131dcae11bf1
Giannicola Olivadoti <olinicola@gmail.com>
2015-06-01 21:56:37 +0200

    Implement SetNX on LRUNoTS and LRU backends

--- lru_nots.go
+++ lru_nots.go
@@ -97,6 +97,39 @@ func (l *LRUNoTS) Set(key string, val interface{}) error {
 	return nil
 }
 
+// SetNX sets a value to the cache only if it does not already exist
+func (l *LRUNoTS) SetNX(key string, val interface{}) (bool, error) {
+	// try to get item
+	_, err := l.cache.Get(key)
+	if err != nil && err != ErrNotFound {
+		return false, err
+	}
+
+	var elem *list.Element
+
+	// if elem is not in the cache, push it to front of the list
+	if err == ErrNotFound {
+		elem = l.list.PushFront(&kv{k: key, v: val})
+	} else {
+		// if elem is already in the cache, do nothing
+		return false, nil
+	}
+
+	// if elem is not in the cache, set the item to the cache
+	err = l.cache.Set(key, elem)
+	if err != nil {
+		return false, err
+	}
+
+	// if the cache is full, evict last entry
+	if l.list.Len() > l.size {
+		// remove last element from cache
+		return true, l.removeElem(l.list.Back())
+	}
+
+	return true, nil
+}
+
 // Delete deletes the given key-value pair from cache, this function doesnt
 // return an error if item is not in the cache
 func (l *LRUNoTS) Delete(key string) error {
