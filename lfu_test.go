729e0000e717a7fdbf5ec125cfb7a2d80c6ae9a1
mehmetalisavas <mehmetali.savas@yandex.com>
2016-08-26 20:31:48 +0300

    benchmark: lfu set and get operations benchmarked

--- lfu_test.go
+++ lfu_test.go
@@ -1,6 +1,9 @@
 package cache
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestLFUNGetSet(t *testing.T) {
 	cache := NewLFU(2)
@@ -34,5 +37,39 @@ func TestLFUEviction(t *testing.T) {
 	_, err = cache.Get("test_key")
 	if err == nil {
 		t.Fatal("test_key should not be in the cache")
+	}
+}
+
+func BenchmarkLFUSet1000(b *testing.B) {
+	cache := NewLFU(5)
+	for n := 0; n < b.N; n++ {
+		testSetNTimes(cache, 1000)
+	}
+}
+func BenchmarkLFUGet1000(b *testing.B) {
+	cache := NewLFU(5)
+	for n := 0; n < b.N; n++ {
+		testGetNTimes(cache, 1000)
+	}
+}
+
+func testSetNTimes(cache Cache, n int) {
+	for i := 0; i < n; i++ {
+		cache.Set("keyBench", i)
+	}
+}
+
+func testGetNTimes(cache Cache, n int) {
+	_, err := cache.Get("keyBench")
+	if err != nil && err != ErrNotFound {
+		fmt.Println("Occurred error while getting from cache")
+		return
+	}
+	if err == ErrNotFound {
+		cache.Set("keyBench", "test")
+	}
+
+	for i := 0; i < n-1; i++ {
+		cache.Get("keyBench")
 	}
 }
